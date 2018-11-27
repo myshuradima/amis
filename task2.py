190ed942a5b4de9c43fd32f44688db0830b6c5aa
myshuradima <44754035+myshuradima@users.noreply.github.com>
2018-11-27 14:18:25 +0200

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,35 @@
+from data import dataset
+
+
+#    Створити пакет validators та написати функції, що валідують усі дані. Імпорутвати дані функції.
+
+from validators.lib import ?
+?
+?
+
+
+from task1 import addStudentSkill
+
+
+#   Написати функцію, що зберігає інформацію про вміння студента у певній компанії
+#   Усі дані вводить користувач. Використати валідатори. Викликати функцію
+
+def addStudentSkillValidator():
+    #TODO
+    ? = getStudent()
+
+
+    ? = getCompany()
+
+    ? = getSkill()
+
+    addStudentSkill(?, ?, ?)
+
+
+
+print("Task 1")
+addStudentSkillValidator()
+print(dataset)
+
+
+print("\n\n")
